8327ca8b20ecbcf60c1e76116a93dd089299df52
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-19 10:56:44 +0530

    Add basic pseudocode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Component } from "react";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  render() {
+    return <div></div>;
+  } 
 }
 export default App;
+
+// * What is the problem?
+// - Write a program that takes user's input & generate a CV which can be downloaded as a PDf file.
+
+// * What interface do we need?
+// - Input page
+// - Preview & Download page
+
+// * What functionality do we need?
+// - Get data
+// - Validate data
+// - Store Data
+// - Generate CV preview
+// - Generate PDF
+// - Autofill input as an example
