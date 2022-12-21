bc1965750ae4fefce3f413eca4fc2c488bb5a62e
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-21 12:20:26 +0530

    Combine  all inputFields on one file

---
+++ src/components/Main/Utils/inputFields.js
@@ -0,0 +1,83 @@
+
+const inputFields = {
+    personal: [
+        {   
+            name: 'first_name',
+            placeholder: 'First Name',
+        },
+        {   
+            name: 'last_name',
+            placeholder: 'Last Name',
+        },
+        {
+            name: 'address',
+            placeholder: 'Address',
+        },
+        {
+            name: 'email',
+            placeholder: 'Email',
+        },
+        {
+            name: 'phone_number',
+            placeholder: 'Phone Number',
+        },
+        {
+            name: 'description',
+            placeholder: 'Tell us something about yourself!',
+        }
+    ],
+    education: [
+        {
+            name: 'university_name',
+            placeholder: 'University Name'
+        },
+        {
+            name: 'city',
+            placeholder: 'City'
+        },
+        {
+            name: 'degree',
+            placeholder: 'Degree'
+        },
+        {
+            name: 'subject',
+            placeholder: 'Subject'
+        },
+        {
+            name: 'from',
+            placeholder: 'From'
+        },
+        {
+            name: 'to',
+            placeholder: 'To'
+        }
+    ],
+    experience: [
+        {
+            name: 'position',
+            placeholder: 'Position'
+        },
+        {
+            name: 'company',
+            placeholder: 'Company'
+        },
+        {
+            name: 'task',
+            placeholder: 'Your main occupation & daily tasks'
+        },
+        {
+            name: 'city',
+            placeholder: 'City'
+        },
+        {
+            name: 'from',
+            placeholder: 'From'
+        },
+        {
+            name: 'to',
+            placeholder: 'To'
+        }
+    ] 
+}
+
+export default inputFields;
