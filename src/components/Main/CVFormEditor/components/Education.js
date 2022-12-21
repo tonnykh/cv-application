460ddbb85f229759ce1568793247d27d6fbc9e9d
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-21 12:27:50 +0530

    Refactor Education component

--- src/components/Main/CVFormEditor/components/Education.js
+++ src/components/Main/CVFormEditor/components/Education.js
@@ -1,60 +1,21 @@
 import Input from './Input';
+import inputFields from '../../Utils/inputFields';
 import uniqid from 'uniqid';
 
-const inputFields = [
-    {
-        name: 'university_name',
-        placeholder: 'University Name'
-    },
-    {
-        name: 'city',
-        placeholder: 'City'
-    },
-    {
-        name: 'degree',
-        placeholder: 'Degree'
-    },
-    {
-        name: 'subject',
-        placeholder: 'Subject'
-    },
-    {
-        name: 'from',
-        placeholder: 'From'
-    },
-    {
-        name: 'to',
-        placeholder: 'To'
-    }
-]
+const education = inputFields.education;
 
-inputFields.forEach(field => field.key = uniqid());
+education.forEach(field => field.key = uniqid());
 
 const Education = (props) => {
    const { onChange } = props;
 
     return (
         <form>
-            {inputFields.map((inputField) => {
-                return <Input placeholder={inputField.placeholder} name={inputField.name} onChange={onChange} key={inputField.key}/>
+            {education.map((educationInput) => {
+                return <Input placeholder={educationInput.placeholder} name={educationInput.name} onChange={onChange} key={educationInput.key}/>
             })}
         </form>
     )
 }
-
-// class Education extends Component {
-//     render() {
-//         return (
-//             <form>
-//                 <input type="text" placeholder="University Name" />
-//                 <input type="text" placeholder="City" />
-//                 <input type="text" placeholder="Degree" />
-//                 <input type="text" placeholder="Subject" />
-//                 <input type="text" placeholder="From" />
-//                 <input type="text" placeholder="To" />
-//             </form>
-//         )
-//     }
-// }
 
 export default Education;
