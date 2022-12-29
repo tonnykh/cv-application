02194d57a46453b1223e15b13e56ff8087dcf1bf
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-12-29 17:20:31 +0530

    Add my github page link

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -1,4 +1,5 @@
 import Button from "../Utils/Button";
+import GithubButton from "../Utils/GithubButton";
 
 const Header = (props) =>  {
     const { autofill, preview, state } = props;
@@ -6,8 +7,17 @@ const Header = (props) =>  {
     return(
         <header>
             <h2>CV Builder</h2>
-            <Button text="Autofill" onClick={autofill} />
-            <Button text={state ? "Editor" : "Preview"} className={"preview-button"} onClick={preview} />
+            <div className="made-by">
+                <h3>Created by </h3>
+                <GithubButton
+                    username="tonny kh"
+                    projectName="cv-application"
+                />
+            </div>
+            <div className="header-buttons">
+                <Button text="Autofill" onClick={autofill} />
+                <Button text={state ? "Editor" : "Preview"} className={"preview-button"} onClick={preview} />
+            </div>
         </header>
     )
 }
