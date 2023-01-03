9c8add11add91b152bae4baf8a07ad9e4b38603d
Khumbongmayum Tonny <tonnykh@icloud.com>
2023-01-03 19:39:39 +0530

    Change class component to functional component on App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Component } from "react";
 import Main from "./components/Main";
-import './styles/App.css';
+import "./styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Main />
-      </div>
-    );
-  } 
-}
+const App = () => (
+  <div className="app">
+    <Main />
+  </div>
+);
 
 export default App;
